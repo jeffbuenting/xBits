dcc64b5cb5a150199f5d0404c07254e8fec9c0f3
jeffbuenting <jeffbuenting@gmail.com>
2017-01-03 08:59:58 -0500

    New XBits DSC Resource

---
+++ xBits.psm1
@@ -0,0 +1,112 @@
+﻿<#
+    .Synopsis
+        bit transfer dsc resource
+
+#>
+
+Enum Ensure {
+    Present
+    Absent
+}
+
+[DscResource()]
+Class xBitTransfer {
+
+    [DSCProperty(Key)]
+    [String]$Url
+
+    [DSCProperty()]
+    [String]$FileName
+    
+    [DSCProperty()]
+    [Bool]$Asynchronous = $False
+
+    [DSCProperty()]
+    [ValidateSet ( 'Basic','Digest','NTLM','Negotiate','Passport' )]
+    [String]$Authentication = 'Negotiate'
+
+    [DSCProperty()]
+    [PSCredential]$Credential
+
+    [DSCProperty()]
+    [String]$Description
+
+    [DSCProperty()]
+    [String]$Destination
+
+    [DSCProperty()]
+    [String]$DisplayName
+
+    [DscProperty()]
+    [ValidateSet ( 'High','Foreground','Normal','Low' ) ]
+    [String]$Priority = 'Normal'
+
+    [DscProperty()]
+    [ValidateSet ( 'Basic','Digest','NTLM','Negotiate','Passport' ) ]
+    [String]$ProxyAuthentication = 'Negotiate'
+
+    [DSCProperty()]
+    [Ensure]$Ensure
+
+
+    [Bool]Test() 
+    {
+
+        $Present = Test-Path -Path "$($This.Destination)\$($This.fileName)"
+
+        if ( $This.Ensure ) 
+        {
+            Return $Present
+        }
+        Else {
+            Return -Not $Present
+        }
+    }
+
+    [xBitTransfer]Get() 
+    {
+        $present = Test-Path -Path "$($This.Destination)\$($This.fileName)"
+
+        if ($present)
+        {
+            $file = Get-Item -Path "$($This.Destination)\$($This.fileName)"
+            $this.FileName = $File.Name
+            $this.Ensure = [Ensure]::Present
+        }
+        else
+        {
+            $this.Ensure = [Ensure]::Absent
+        }
+
+        return $this
+    }
+
+    [Void]Set() 
+    {
+        if ( $This.Credential ) 
+        {
+            Write-Verbose "Downloading via Bits with Credential"
+            Start-BitsTransfer -source $This.Url `
+                -Destination $This.Destination `
+                -Asynchronous:$This.Asynchronous `
+                -Authentication $This.Authentication `
+                -Credential $This.Credential `
+                -Description $This.Description `
+                -DisplayName $This.DisplayName `
+                -Priority $This.Priority `
+        }
+        Else 
+        {
+            Write-Verbose "Downloading via Bits"
+            Start-BitsTransfer -source $This.Url `
+                -Destination $This.Destination `
+                -Asynchronous:$This.Asynchronous `
+                -Authentication $This.Authentication `
+                -Description $This.Description `
+                -DisplayName $This.DisplayName `
+                -Priority $This.Priority 
+        }
+            
+               
+    }
+}
