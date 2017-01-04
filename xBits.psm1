01cd1c6ca2d640d5148c6b6e2d7bb71d22e6beb7
jeffbuenting <jeffbuenting@gmail.com>
2017-01-04 13:49:41 -0500

    Fixed manifest file so the resource was actually seen
    BitsTransfer works

--- xBits.psm1
+++ xBits.psm1
@@ -1,7 +1,5 @@
 ﻿<#
-    .Synopsis
-        bit transfer dsc resource
-
+    DSC Class Resource to copy files via Bits Transfer
 #>
 
 Enum Ensure {
@@ -12,12 +10,12 @@ Enum Ensure {
 [DscResource()]
 Class xBitTransfer {
 
-    [Parameter (Mandatory = $True)]
-    [DSCProperty(Key)]
+    # ----- Path / web address to the file
+    [DSCProperty(Mandatory)]
     [String]$Url
 
-    [Parameter (Mandatory = $True)]
-    [DSCProperty()]
+    # ----- Name of the file that combines with URL for the full path to the source.
+    [DSCProperty(Key)]
     [String]$FileName
     
     [DSCProperty()]
@@ -31,14 +29,13 @@ Class xBitTransfer {
     [PSCredential]$Credential
 
     [DSCProperty()]
-    [String]$Description = $FileName
+    [String]$Description 
 
-    [Parameter (Mandatory = $True)]
-    [DSCProperty()]
+    [DSCProperty(Mandatory)]
     [String]$Destination
 
     [DSCProperty()]
-    [String]$DisplayName = $FileName
+    [String]$DisplayName 
 
     [DscProperty()]
     [ValidateSet ( 'High','Foreground','Normal','Low' ) ]
@@ -49,23 +46,26 @@ Class xBitTransfer {
     [String]$ProxyAuthentication = 'Negotiate'
 
     [DSCProperty()]
-    [Ensure]$Ensure
+    [Ensure]$Ensure = 'Present'
 
-
+    # ----- Test if the file exists.  
     [Bool]Test() 
     {
-
+        
         $Present = Test-Path -Path "$($This.Destination)\$($This.fileName)"
 
-        if ( $This.Ensure ) 
+        if ( $This.Ensure -eq 'Present' ) 
         {
+            Write-Verbose "We want $($This.Filename) to exist and it does : $Present"
             Return $Present
         }
         Else {
+            Write-Verbose "We don't want $($This.Filename) to exist and it doesn't : $(-Not $Present)"
             Return -Not $Present
         }
     }
 
+    # ----- Returns info about the file at the destination if it exists.
     [xBitTransfer]Get() 
     {
         $present = Test-Path -Path "$($This.Destination)\$($This.fileName)"
@@ -78,18 +78,37 @@ Class xBitTransfer {
         }
         else
         {
+            $This.FileName = $Null
+            $This.Url = $Null
             $this.Ensure = [Ensure]::Absent
         }
 
         return $this
     }
 
+    # ----- Performs Bit Transfer if file does not exist at destination
     [Void]Set() 
     {
+        Write-Verbose "Downloading File : $($This.fileName), from : $($This.Url), to : $($This.Destination)"
+
+        # ----- Because we get errors with $Null.  Converting $Null to $This.Filename.
+        if ( $This.Description -eq $Null ) { $This.Description = $This.FileName }
+        if ( $This.DisplayName -eq $Null ) { $This.DisplayName = $This.FileName }
+
+        Write-Verbose "Description : $($This.Description)"
+        Write-Verbose "DisplayName : $($This.DisplayName)"
+        Write-Verbose "Anychronous : $($This.Asynchronous)"
+        Write-Verbose "Authentication : $($This.Authentication)"
+        Write-Verbose "Priority : $($This.Priority)"
+        Write-Verbose "Url : $($This.Url)"
+        Write-Verbose "FileName : $($This.FileName)"
+        Write-Verbose "URL/FileName : $($This.Url)/$($This.FileName)"
+
+        
         if ( $This.Credential ) 
         {
             Write-Verbose "Downloading via Bits with Credential"
-            Start-BitsTransfer -source $This.Url `
+            Start-BitsTransfer -source "$($This.Url)/$($This.FileName)" `
                 -Destination $This.Destination `
                 -Asynchronous:$This.Asynchronous `
                 -Authentication $This.Authentication `
@@ -97,17 +116,20 @@ Class xBitTransfer {
                 -Description $This.Description `
                 -DisplayName $This.DisplayName `
                 -Priority $This.Priority `
+                -ErrorAction Stop `
+                -Verbose
         }
         Else 
         {
             Write-Verbose "Downloading via Bits"
-            Start-BitsTransfer -source $This.Url `
+            Start-BitsTransfer -Source "$($This.Url)/$($This.FileName)" `
                 -Destination $This.Destination `
                 -Asynchronous:$This.Asynchronous `
                 -Authentication $This.Authentication `
                 -Description $This.Description `
                 -DisplayName $This.DisplayName `
-                -Priority $This.Priority 
-        }              
+                -Priority $This.Priority `
+                -ErrorAction Stop
+        }  
     }
 }
